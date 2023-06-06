233627ad52cc88914757b2d18909571235d13bdf
Quinn <quinn.linares@gmail.com>
2023-06-05 21:45:51 -0400

    wokring on character creation

---
+++ public/js/creation.js
@@ -0,0 +1,10 @@
+var elem = document.getElementById('creation');
+
+function charCreator(elem, onclick) {
+if (elem.style.display = display){
+elem.style.display = 'none';
+}
+else {
+elem.style.display = 'display';
+}
+}
